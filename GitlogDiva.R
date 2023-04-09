f107bc55c9406ab0fcf411f9bd0e5ace0b6cb439
Diva <miuixtli@yahoo.com>
2023-04-08 19:57:12 -0700

    Added complete git log list.

--- GitlogDiva.R
+++ GitlogDiva.R
@@ -23,8 +23,32 @@ Body:
   
   Subject: Complete Objective 2 program.
 Author: Diva
+Date: Sat, 8 Apr 2023 14:08:58 -0700
+Body: 
   
-
-
-
+  Subject: Completed Objective_1.
+Author: Diva
+Date: Sat, 8 Apr 2023 13:50:00 -0700
+Body: 
+  
+  Subject: Update README.md
+Author: Wesley Newcomb
+Date: Fri, 7 Apr 2023 19:20:29 -0700
+Body: 
+  
+  Subject: Add files via upload
+Author: Wesley Newcomb
+Date: Fri, 7 Apr 2023 19:18:24 -0700
+Body: 
+  
+  Subject: Add files via upload
+Author: Wesley Newcomb
+Date: Fri, 7 Apr 2023 19:14:55 -0700
+Body: 
+  
+  Subject: Initial commit
+Author: Wesley Newcomb
+Date: Fri, 7 Apr 2023 19:05:40 -0700
+Body: 
+  (END)
 
